b625d743ec83e63d1033d7a52b01efdf6c9f7855
Rhythm <58517101+rhythm1920@users.noreply.github.com>
2020-09-17 15:15:33 +0530

    Set up mongoDB

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var dotenv = require("dotenv");
+var cors = require("cors");
+let Schema = mongoose.Schema;
+const bodyParser = require("body-parser");
 
 var app = express();
-
-// Basic Configuration 
+dotenv.config();
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(
+  process.env.DB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to MongoDB")
+);
+
+//create url model
+let urlSchema = new Schema({
+  url: { type: String, required: true },
+  shortUrl: String,
+});
+
+const Url = mongoose.model("Url", urlSchema);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); // mounting body-parser here
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
+  console.log("View the app here: http://localhost:" + port);
 });
